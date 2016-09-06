cd69eb25640f40d18c9018e06a738dee1aba8415
Coy Sanders <coymeetsworld@gmail.com>
2016-09-06 01:59:47 -0700

    DataVis: Scatterplot: Added content and style to tooltip to show details on plot points.

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -26,6 +26,41 @@ $(document).ready(function() {
 				 .text("Ranking");
 	}
 
+	function hideTooltip() {
+		d3.select("#tooltip").attr("fill-opacity", 0);
+  }
+
+	function formatTooltipText(data) {
+		var text = "<tspan x=50% y=25 style=\"font-weight: bold;\">" + data.Name + " " + data.Nationality + "</tspan>";
+		text    += "<tspan x=50% y=45 style=\"font-style: italic;\">" + data.Year + ": " + data.Time + "</tspan>";
+
+		var y = 75;
+		var dopingDesc = data.Doping.split(" ");
+		var lineWidth = 20;
+		console.log(dopingDesc);
+		var word = "";
+		for (var i = 0; i < dopingDesc.length; i++) {
+			if (word.length + dopingDesc[i].length > lineWidth + 1) {
+				console.log("Building word: " + word);
+				text += "<tspan x=50% y=" + y + ">" + word + "</tspan>";
+				word = "";
+				y += 15;
+			} 
+			word += dopingDesc[i] + " ";
+		}
+		if (word) { text += "<tspan x=50% y=" + y +">" + word + "</tspan>"; }
+
+		return text;
+	}
+
+  function renderTooltip(data) {
+		d3.select("#tooltip").attr("fill-opacity", 1);
+		var color = data.Doping ? "lightpink" : "lime";
+		d3.select("#tooltip").attr("fill", color);
+		d3.select("#tooltip text").html(formatTooltipText(data));
+
+  }
+
 
 	var chartWidth = 1000;
 	var chartHeight = 500;
@@ -58,7 +93,14 @@ $(document).ready(function() {
        .attr("r", 5)
        .attr("cx", function(d) { return x(d.Seconds-min); })
        .attr("cy", function(d) { return y(d.Place); })
-       .attr("fill", function(d) { return d.Doping  ? "red" : "lime"; });
+       .attr("fill", function(d) { return d.Doping  ? "red" : "lime"; })
+			 .on("mouseout", function() {
+				hideTooltip();
+			 })
+			 .on("mouseover", function() {
+		     renderTooltip(d3.select(this).datum());
+			 });
+
 
 	  elemEnter.append("text")
 				.attr("dx", function(d){return x(d.Seconds-min-3)})
