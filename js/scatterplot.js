92d336895045cb2fd531ff4abfc6962f0d55248f
Coy Sanders <coymeetsworld@gmail.com>
2016-09-05 23:33:45 -0700

    DataVis: Scatterplot: Minor refactoring, changed page title.

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -7,9 +7,8 @@ $(document).ready(function() {
 
 		//Add text label for x-axis
 		chart.append("text")
-				 .attr("transform", "translate(" + (chartWidth/2) + "," + (chartHeight+20+20) + ")")
-				 .style("text-anchor", "middle")
-				 .style("color", "white")
+				 .attr("transform", "translate(" + (chartWidth/2) + "," + (chartHeight+50) + ")")
+				 .attr("class", "axisLabel")
 				 .text("Minutes behind Fastest Time");
 	}
 
@@ -20,10 +19,10 @@ $(document).ready(function() {
 		//Add Text label for the y-axis
 		chart.append("text")
 				 .attr("transform", "rotate(-90)")
-				 .attr("y", -70)
-				 .attr("x", -25)
+				 .attr("y", -75)
+				 .attr("x", -50)
+				 .attr("class", "axisLabel")
 				 .attr("dy", "2em")
-				 .style("text-anchor", "middle")
 				 .text("Ranking");
 	}
 
@@ -33,41 +32,33 @@ $(document).ready(function() {
 
 	var x = d3.scaleTime().range([0, chartWidth]);
 	var y = d3.scaleLinear().range([chartHeight, 0]);
+  var formatTime = d3.timeFormat("%M:%S"),
+		formatSeconds = function(d) { return formatTime(new Date(2016, 0, 1, 0, 0, d)); };
+
 	var chart = d3.select('.chart').attr("width", chartWidth).attr("height", chartHeight).append("g").attr("transform", "translate(" + 0 + "," + 0 + ")");
 
 	d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', function(error, cyclistData) {
 
 		if (error) throw error;
 
-		console.log(cyclistData);
 		var lowestPlace = d3.max(cyclistData, function(d) { return d.Place });
 		y.domain([lowestPlace+2, 1]);
-		console.log(d3.extent(cyclistData, function(d) { return d.Seconds }));
 
-		var formatTime = d3.timeFormat("%M:%S"),
-			formatSeconds = function(d) { return formatTime(new Date(2016, 0, 1, 0, 0, d)); };
 		var min = d3.min(cyclistData, function(d) { return d.Seconds} );
 		var max = d3.max(cyclistData, function(d) { return d.Seconds} );
 		x.domain([max-min+15, -15]);
-
-		var shortestTime = formatSeconds(min);
-		var longestTime = formatSeconds(max);
-		console.log("Max: " + max);
-		console.log("Min: " + min);
-		console.log("Shortest Time: " + shortestTime);
-		console.log("Longest Time: " + longestTime);
 
 		buildYAxis(y);
 		buildXAxis(x, formatSeconds);
 
 		var elem = chart.selectAll("cyclist").data(cyclistData);
-
 		var elemEnter = elem.enter().append("g");
 
 		elemEnter.append("circle")
        .attr("r", 5)
        .attr("cx", function(d) { return x(d.Seconds-min); })
        .attr("cy", function(d) { return y(d.Place); })
+       .attr("fill", function(d) { return d.Doping  ? "red" : "lime"; });
 
 	  elemEnter.append("text")
 				.attr("dx", function(d){return x(d.Seconds-min-3)})
