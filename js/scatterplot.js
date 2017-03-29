21acb0eeac7085bdface279ce74e7c8332b2061e
Coy Sanders <coymeetsworld@gmail.com>
2017-03-28 20:00:12 -0700

    Refactored using ES6 and Google style guide

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,109 +1,108 @@
-$(document).ready(function() {
 
-	function buildXAxis(x, callback) {
-		chart.append("g")
-				 .attr("transform", "translate(0," + chartHeight + ")")
-				 .call(d3.axisBottom(x).tickFormat(callback));
+const CHART_WIDTH = 1000;
+const CHART_HEIGHT = 500;
+const FORMAT_TIME = d3.timeFormat("%M:%S");
+const FORMAT_SECONDS = d => FORMAT_TIME(new Date(2016, 0, 1, 0, 0, d));
 
-		//Add text label for x-axis
-		chart.append("text")
-				 .attr("transform", "translate(" + (chartWidth/2) + "," + (chartHeight+50) + ")")
-				 .attr("class", "axisLabel")
-				 .text("Minutes behind Fastest Time");
-	}
+let chart = d3.select('.chart')
+              .attr("width", CHART_WIDTH)
+              .attr("height", CHART_HEIGHT)
+              .append("g")
+              .attr("transform", "translate(0,0)");
 
+let x = d3.scaleTime().range([0, CHART_WIDTH]);
+let y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
 
-	function buildYAxis(y) {
-		chart.append("g").attr("transform", "translate(0,0)").call(d3.axisLeft(y));
+const buildXAxis = (x, callback) => {
+  chart.append("g")
+       .attr("transform", `translate(0,${CHART_HEIGHT})`)
+       .call(d3.axisBottom(x).tickFormat(callback));
 
-		//Add Text label for the y-axis
-		chart.append("text")
-				 .attr("transform", "rotate(-90)")
-				 .attr("y", -75)
-				 .attr("x", -50)
-				 .attr("class", "axisLabel")
-				 .attr("dy", "2em")
-				 .text("Ranking");
-	}
+  chart.append("text")
+       .attr("transform", `translate(${CHART_WIDTH/2},${CHART_HEIGHT+50})`)
+       .attr("class", "axis-label")
+       .text("Minutes behind Fastest Time");
+}
 
+const buildYAxis = (y) => {
+  chart.append("g").attr("transform", "translate(0,0)").call(d3.axisLeft(y));
 
-	function formatTooltipText(data) {
-		var text = "<tspan x=50% y=25 style=\"font-weight: bold;\">" + data.Name + " " + data.Nationality + "</tspan>";
-		text    += "<tspan x=50% y=45 style=\"font-style: italic;\">" + data.Year + ": " + data.Time + "</tspan>";
+  chart.append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", -75)
+       .attr("x", -50)
+       .attr("class", "axis-label")
+       .attr("dy", "2em")
+       .text("Ranking");
+}
 
-		var y = 75;
-		var dopingDesc = data.Doping.split(" ");
-		var lineWidth = 20;
-		var word = "";
-		for (var i = 0; i < dopingDesc.length; i++) {
-			if (word.length + dopingDesc[i].length > lineWidth + 1) {
-				text += "<tspan x=50% y=" + y + ">" + word + "</tspan>";
-				word = "";
-				y += 15;
-			} 
-			word += dopingDesc[i] + " ";
-		}
-		if (word) { text += "<tspan x=50% y=" + y +">" + word + "</tspan>"; }
+const formatTooltipText = (data) => {
+  const LINE_WIDTH = 20;
+  
+  let text = `<tspan x=50% y=25 style="font-weight: bold;">${data.Name} ${data.Nationality}</tspan>`;
+  text += `<tspan x=50% y=45 style="font-style: italic;"> ${data.Year}:${data.Time}</tspan>`;
 
-		return text;
-	}
+  let y = 75;
+  let line = "";
 
+  data.Doping.split(" ").map( (word) => {
+    if (line.length + word.length > LINE_WIDTH + 1) { //1 is for the space
+      text += `<tspan x=50% y=${y}>${line}</tspan>`;
+      line = "";
+      y += 15; // Move next line down
+    }
+    line += word + " ";
+  });
 
-	function renderTooltip(data) {
-		d3.select("#tooltip").attr("fill-opacity", 1);
-		var color = data.Doping ? "lightpink" : "lime";
-		d3.select("#tooltip").attr("fill", color);
-		d3.select("#tooltip text").html(formatTooltipText(data));
-	}
+  if (line) { 
+    text += `<tspan x=50% y=${y}>${line}</tspan>`;
+  }
 
+  return text;
+}
 
-	function hideTooltip() {
-		d3.select("#tooltip").attr("fill-opacity", 0);
-	}
+const renderTooltip = (data) => {
+  d3.select("#tooltip").attr("fill-opacity", 1);
+  let color = data.Doping ? "lightpink" : "lime";
+  d3.select("#tooltip").attr("fill", color);
+  d3.select("#tooltip text").html(formatTooltipText(data));
+}
 
-
-	var chartWidth = 1000;
-	var chartHeight = 500;
-	var chart = d3.select('.chart').attr("width", chartWidth).attr("height", chartHeight).append("g").attr("transform", "translate(" + 0 + "," + 0 + ")");
-
-	var x = d3.scaleTime().range([0, chartWidth]);
-	var y = d3.scaleLinear().range([chartHeight, 0]);
-	var formatTime = d3.timeFormat("%M:%S")
-	var formatSeconds = function(d) { return formatTime(new Date(2016, 0, 1, 0, 0, d)); };
-
-	d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', function(error, cyclistData) {
-
-		if (error) throw error;
-
-		var lowestPlace = d3.max(cyclistData, function(d) { return d.Place });
-		y.domain([lowestPlace+2, 1]);
-
-		var min = d3.min(cyclistData, function(d) { return d.Seconds} );
-		var max = d3.max(cyclistData, function(d) { return d.Seconds} );
-		x.domain([max-min+15, -15]);
-
-		buildYAxis(y);
-		buildXAxis(x, formatSeconds);
-
-		var elem = chart.selectAll("cyclist").data(cyclistData);
-		var elemEnter = elem.enter().append("g");
-
-		elemEnter.append("circle")
-						 .attr("r", 5)
-						 .attr("cx", function(d) { return x(d.Seconds-min); })
-						 .attr("cy", function(d) { return y(d.Place); })
-						 .attr("fill", function(d) { return d.Doping  ? "red" : "lime"; })
-						 .on("mouseout", function() {
-							hideTooltip();
-						 })
-						 .on("mouseover", function() {
-							renderTooltip(d3.select(this).datum());
-						 });
+const hideTooltip = () => {
+  d3.select("#tooltip").attr("fill-opacity", 0);
+}
 
 
-		elemEnter.append("text")
-						 .attr("dx", function(d){return x(d.Seconds-min-3)})
-						 .attr("dy", function(d){return y(d.Place+0.25)})
-						 .text(function(d){return d.Name});
-	});
+d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', (error, cyclistData) => {
+
+  if (error) throw error;
+
+  let lowestPlace = d3.max(cyclistData, d => d.Place);
+  y.domain([lowestPlace+2, 1]);
+
+  const MIN = d3.min(cyclistData, d => d.Seconds);
+  const MAX = d3.max(cyclistData, d => d.Seconds);
+
+  x.domain([MAX-MIN+15, -15]);
+
+  buildYAxis(y);
+  buildXAxis(x, FORMAT_SECONDS);
+
+  let elem = chart.selectAll("cyclist").data(cyclistData);
+  let elemEnter = elem.enter().append("g");
+
+  elemEnter.append("circle")
+           .attr("r", 5)
+           .attr("cx", d => x(d.Seconds-MIN))
+           .attr("cy", d => y(d.Place))
+           .attr("fill", d => d.Doping ? "red" : "lime")
+           .on("mouseout", () => hideTooltip())
+           .on("mouseover", function() {
+             renderTooltip(d3.select(this).datum());
+           });
+
+  elemEnter.append("text")
+           .attr("dx", d => x(d.Seconds-MIN-3))
+           .attr("dy", d => y(d.Place+0.25))
+           .text(d => d.Name);
 });
